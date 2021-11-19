961faa2e1bbebcc725d56cbae5907edb19832385
Sherrelyn <chavezsherrelyn13@gamil.com>
2021-11-19 08:51:32 -0800

    Added function and methods. Created variables. Added if statement and parameters. Added a string to build a url. All changes added to app.js.

---
+++ js/app.js
@@ -0,0 +1,43 @@
+document.querySelector('#generate-names').addEventListener('submit', loadNames);
+
+
+
+
+
+//Execute the function to query the API 
+function loadNames(e) {
+    e.preventDefault();
+
+
+    //Read the values from the form and create a variables 
+    const origin = document.getElementById('country').value;
+    const genre = document.getElementById('genre').value;
+    const amount = document.getElementById('quantity').value;
+
+    //Build the URL
+    let url = 'http://uinames.com/api/?';
+    //Read the origin and append to the URL
+    if(origin !== ''){
+        url += `?region=${origin}&`;
+
+    }
+
+    //Read the genre and append to the URL
+    if(genre !== ''){
+        url += `?gender=${genre}&`;
+
+    }
+    
+     //Read the amount and append to the URL
+    if(amount !== ''){
+      url += `?amount=${amount}&`;
+    
+
+
+    }
+
+    console.log(url);
+
+
+
+}
