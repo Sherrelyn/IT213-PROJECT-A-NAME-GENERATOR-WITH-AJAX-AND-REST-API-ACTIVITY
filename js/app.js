b813fab1309514a422aa67937c5abb1fafec6212
Sherrelyn <chavezsherrelyn13@gamil.com>
2021-11-19 09:20:37 -0800

    Added a method to make the Ajax request. Added if statement to execute a function. Created a variable name and pass it. All changes added to app.js.

--- js/app.js
+++ js/app.js
@@ -18,6 +18,31 @@ function loadNames(e) {
      if(origin !== '') {
          url += 'region=${origin}&';
      }
+          // Read the genre and append the url
+          if(genre !== '') {
+            url += 'gender=${genre}&';
+        }
+             // Read the amount  and append the url
+     if(amount !== '') {
+        url += 'amount=${amount}&';
+    }
+
+    // Ajax call
+    const xhr = new XMLHttpRequest();
+
+    //Open the connection
+    xhr.open('GET', url, true);
+
+    //Execute the function
+    xhr.onload = function() {
+        if(this.status === 200) {
+            const names = JSON.parse(this.responseText);
+        }
+    }
+
+
+    //Send the request
+    xhr.send();
  }
 
 
